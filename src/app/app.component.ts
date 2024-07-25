a023a86671d5df9a436f82ba3991318396f2680d
Utsav-Sharma33 <utsav.sharma56@gmail.com>
2024-07-25 13:25:19 +0530

    added bootstrap added increment/decrement functionality added dummy navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { CounterPage } from '../pages/counter/counter.page';
+import { CounterPage } from './pages/counter/counter.page';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CounterPage],
+  imports: [CounterPage, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import 'zone.js';
-import { AppComponent } from './app/app/app.component';
+import { AppComponent } from './app/app.component';
 import { provideStore } from '@ngrx/store';
 import { counterReducer } from './app/store/counter/counter.reducer';
 
